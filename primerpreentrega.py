a44fb1117d5c1de141e4754a042ab3bfc2a12d8f
keviin00 <90279937+keviin00@users.noreply.github.com>
2023-08-23 20:49:05 -0300

    Add files via upload

--- primerpreentrega.py
+++ primerpreentrega.py
@@ -1,10 +1,26 @@
+import json
+
 # Diccionario para almacenar información de usuario y contraseña
 usuarios = {}
+
+# Cargar datos de usuarios desde un archivo JSON (si existe)
+def cargar_usuarios():
+    try:
+        with open('usuarios.json', 'r') as archivo:
+            usuarios.update(json.load(archivo))
+    except FileNotFoundError:
+        pass
+
+# Guardar datos de usuarios en un archivo JSON
+def guardar_usuarios():
+    with open('usuarios.json', 'w') as archivo:
+        json.dump(usuarios, archivo)
 
 # Función para registrar un usuario
 def registrar_usuario(usuario, contraseña):
     if usuario not in usuarios:
         usuarios[usuario] = contraseña
+        guardar_usuarios()
         print("Registro exitoso.")
     else:
         print("El usuario ya existe.")
@@ -18,13 +34,16 @@ def mostrar_usuarios():
 # Función para realizar el inicio de sesión
 def iniciar_sesion(usuario, contraseña):
     if usuario in usuarios and usuarios[usuario] == contraseña:
-        print("Inicio de sesión exitoso.",usuario)
+        print("Inicio de sesión exitoso bienvenido",usuario)
     else:
         print("Credenciales incorrectas.")
 
 # Función principal del programa
 def main():
+    cargar_usuarios()
+    
     while True:
+        print("Bienvenido a nuestra pagina")
         print("1. Registrar usuario")
         print("2. Mostrar usuarios")
         print("3. Iniciar sesión")
@@ -42,6 +61,7 @@ def main():
             contraseña = input("Ingresa la contraseña: ")
             iniciar_sesion(usuario, contraseña)
         elif opcion == "4":
+            guardar_usuarios()
             print("¡Hasta luego!")
             break
         else:
@@ -49,5 +69,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
